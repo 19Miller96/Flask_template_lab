a5660dcddb7046376bb9d563a39035ea51b08dac
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-03 15:26:09 +0000

    Third push

--- app/controller.py
+++ app/controller.py
@@ -5,13 +5,30 @@ from app.models.event import *
 
 @app.route('/')
 def index():
-    return "Hello, world!"
-    # return render_template('index.html') #, title='Home', tasks=tasks)
+    return render_template('index.html', title='Home ', events=events)
 
-# @app.route('/add-task', methods=['POST'])
-# def add_task():
-#     task_title = request.form['title']
-#     task_desc = request.form['description']
-#     new_task = Task(task_title, task_desc, False)
-#     add_new_task(new_task)
-#     return render_template('index.html', title='Home', tasks=tasks)
+@app.route('/add-event', methods=['POST'])
+def add_event():
+    date = request.form['event_date']
+    split_date = date.split('-')
+
+# def Convert(string): 
+#     li = list(string.split("-")) 
+#     return li 
+
+# ---- Keith's slack suggestion below ----
+# date = request.form['event_date']
+# split_date = date.split('-')
+# date = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+# ----------------------------------------
+
+    # my_date = datetime.strptime(request.form['event_date'], "%Y-%M-%D")
+    
+    event_date = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+    event_name = request.form['name']
+    event_guests = request.form['number_of_guests']
+    event_location = request.form['room_location']
+    event_description = request.form['description']
+    new_event = Event(event_date, event_name, event_guests, event_location, event_description)
+    add_new_event(new_event)
+    return render_template('index.html', title='Home', events=events)
--- app/models/event_list.py
+++ app/models/event_list.py
@@ -1,9 +1,14 @@
-# from app.models.event import *
+from app.models.event import *
+import datetime
 
-# task1 = Task("Buy groceries", "Milk, Cheese, Pizza, Fruit", False)
-# task2 = Task("Learn Python", "Learn an awesome new programming language", True)
-# tasks = [task1, task2]
+event1 = Event(datetime.date(2020, 12, 24), "Christmas Ball", 200, "Hilton", "The biggest event of the year!")
+event2 = Event(datetime.date(2020, 12, 25), "Christmas Hangover Breakfast", 100, "Hilton", "The second biggest event of the year!")
+events = [event1, event2]
 
 
-# def add_new_task(task):
-#     tasks.append(task)
+def add_new_event(event):
+    events.append(event)
+
+# event1 = Event(datetime.date(2020, 5, 17), "Staff Meeting", 35,  True, 'EDI1', "All staff meeting")
+# event2 = Event(datetime.date(2020, 6, 18), "1-1 review", 2,  False, 'GLA3', "1-1 Review for weekend Homework")
+# event3 = Event(datetime.date(2020, 7, 19), "Quiz Night", 40,  False, 'Open Area', "Quiz night for all cohorts")
